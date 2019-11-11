82dd3073d5abebae198f6cc77f7a53472bad5bbf
Rye In Seong <insung1217@gmail.com>
2019-11-11 20:02:35 +0900

    Grade.java 제대로 된 거 올림

--- app/src/main/java/com/example/onlinelecturefairy/grade/Grade.java
+++ app/src/main/java/com/example/onlinelecturefairy/grade/Grade.java
@@ -1,4 +1,40 @@
 package com.example.onlinelecturefairy.grade;
 
 public class Grade {
+    String lecture;
+    String score;
+    String title;
+    String subId;
+
+    public String getLecture() {
+        return lecture;
+    }
+
+    public void setLecture(String lecture) {
+        this.lecture = lecture;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubId() {
+        return subId;
+    }
+
+    public void setSubId(String subId) {
+        this.subId = subId;
+    }
 }
