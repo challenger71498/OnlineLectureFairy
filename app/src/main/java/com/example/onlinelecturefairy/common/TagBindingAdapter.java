486ef60e5925d78f052ea96a2d347c2bb0205af3
Min Oh <challenger71498@gmail.com>
2019-11-14 05:29:21 +0900

    휴강 update

--- app/src/main/java/com/example/onlinelecturefairy/common/TagBindingAdapter.java
+++ app/src/main/java/com/example/onlinelecturefairy/common/TagBindingAdapter.java
@@ -1,14 +1,23 @@
 package com.example.onlinelecturefairy.common;
 
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.databinding.BindingAdapter;
 
+import com.example.onlinelecturefairy.R;
 import com.example.onlinelecturefairy.ui.tag.TagViewModel;
 
 public class TagBindingAdapter {
     @BindingAdapter("setTagTitle")
     public static void setTagTitle(TextView v, TagViewModel m) {
         v.setText(m.getName().getValue());
+    }
+
+    @BindingAdapter("setTagColor")
+    public static void setTagColor(View v, TagViewModel m) {
+        if(m.getName().getValue().equals("휴강")) {
+            v.setBackgroundResource(R.color.colorPrimary);
+        }
     }
 }
